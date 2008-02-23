f44641829817a7e1192a45b07ca6b0f7b2898d0a
merobbins <merobbins@f2e18c2a-3941-0410-a48b-ebcc20576c27>
2008-02-23 04:30:05 +0000

    Forgot version file.

--- lua/AI/CustomAIs/SorianAI.lua
+++ lua/AI/CustomAIs/SorianAI.lua
@@ -9,37 +9,37 @@
 AIList = {
     {
         key = 'sorian',
-        name = "AI: Sorian 0.9.8e",
+        name = "AI: Sorian 0.9.9",
     },
     {
         key = 'sorianrush',
-        name = "AI: Sorian Rush 0.9.8e",
+        name = "AI: Sorian Rush 0.9.9",
     },
     {
         key = 'sorianair',
-        name = "AI: Sorian Air 0.9.8e",
+        name = "AI: Sorian Air 0.9.9",
     },
     {
         key = 'sorianwater',
-        name = "AI: Sorian Water 0.9.8e",
+        name = "AI: Sorian Water 0.9.9",
     },
 }
 
 CheatAIList = {
     {
         key = 'soriancheat',
-        name = "AIx: Sorian 0.9.8e",
+        name = "AIx: Sorian 0.9.9",
     },
     {
         key = 'sorianrushcheat',
-        name = "AIx: Sorian Rush 0.9.8e",
+        name = "AIx: Sorian Rush 0.9.9",
     },
     {
         key = 'sorianaircheat',
-        name = "AIx: Sorian Air 0.9.8e",
+        name = "AIx: Sorian Air 0.9.9",
     },
     {
         key = 'sorianwatercheat',
-        name = "AIx: Sorian Water 0.9.8e",
+        name = "AIx: Sorian Water 0.9.9",
     },
 }
