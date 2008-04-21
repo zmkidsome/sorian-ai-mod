963f870f7df49a5fe63b66adea91deb69066f081
merobbins <merobbins@f2e18c2a-3941-0410-a48b-ebcc20576c27>
2008-04-21 04:35:55 +0000

    Fixed end of game bug.

--- lua/AI/CustomAIs/SorianAI.lua
+++ lua/AI/CustomAIs/SorianAI.lua
@@ -9,53 +9,53 @@
 AIList = {
     {
         key = 'sorian',
-        name = "AI: Sorian 1.1",
+        name = "AI: Sorian 1.1b",
     },
     {
         key = 'sorianrush',
-        name = "AI: Sorian Rush 1.1",
+        name = "AI: Sorian Rush 1.1b",
     },
     {
         key = 'sorianair',
-        name = "AI: Sorian Air 1.1",
+        name = "AI: Sorian Air 1.1b",
     },
     {
         key = 'sorianwater',
-        name = "AI: Sorian Water 1.1",
+        name = "AI: Sorian Water 1.1b",
     },
     {
         key = 'sorianturtle',
-        name = "AI: Sorian Turtle 1.1",
+        name = "AI: Sorian Turtle 1.1b",
     },
     {
         key = 'sorianadaptive',
-        name = "AI: Sorian Adaptive 1.1",
+        name = "AI: Sorian Adaptive 1.1b",
     },
 }
 
 CheatAIList = {
     {
         key = 'soriancheat',
-        name = "AIx: Sorian 1.1",
+        name = "AIx: Sorian 1.1b",
     },
     {
         key = 'sorianrushcheat',
-        name = "AIx: Sorian Rush 1.1",
+        name = "AIx: Sorian Rush 1.1b",
     },
     {
         key = 'sorianaircheat',
-        name = "AIx: Sorian Air 1.1",
+        name = "AIx: Sorian Air 1.1b",
     },
     {
         key = 'sorianwatercheat',
-        name = "AIx: Sorian Water 1.1",
+        name = "AIx: Sorian Water 1.1b",
     },
     {
         key = 'sorianturtlecheat',
-        name = "AIx: Sorian Turtle 1.1",
+        name = "AIx: Sorian Turtle 1.1b",
     },
     {
         key = 'sorianadaptivecheat',
-        name = "AIx: Sorian Adaptive 1.1",
+        name = "AIx: Sorian Adaptive 1.1b",
     },
 }
