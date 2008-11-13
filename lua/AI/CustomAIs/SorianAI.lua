a66b4ad5f651c7e99eae1d60178066b5d59eb857
merobbins <merobbins@f2e18c2a-3941-0410-a48b-ebcc20576c27>
2008-11-13 22:02:24 +0000

    Anti-Nukes are no longer a requirement for Arty, Nukes, or T3 Firebases. AI should use more engineers to rush build an Anti-Nuke if needed. AI should use intys like before. Hopefully fixed Rush stall.

--- lua/AI/CustomAIs/SorianAI.lua
+++ lua/AI/CustomAIs/SorianAI.lua
@@ -9,53 +9,53 @@
 AIList = {
     {
         key = 'sorian',
-        name = "AI: SAI 1.7.3c",
+        name = "AI: SAI 1.7.3d",
     },
     {
         key = 'sorianrush',
-        name = "AI: SAI Rush 1.7.3c",
+        name = "AI: SAI Rush 1.7.3d",
     },
     {
         key = 'sorianair',
-        name = "AI: SAI Air 1.7.3c",
+        name = "AI: SAI Air 1.7.3d",
     },
     {
         key = 'sorianwater',
-        name = "AI: SAI Water 1.7.3c",
+        name = "AI: SAI Water 1.7.3d",
     },
     {
         key = 'sorianturtle',
-        name = "AI: SAI Turtle 1.7.3c",
+        name = "AI: SAI Turtle 1.7.3d",
     },
     {
         key = 'sorianadaptive',
-        name = "AI: SAI Adaptive 1.7.3c",
+        name = "AI: SAI Adaptive 1.7.3d",
     },
 }
 
 CheatAIList = {
     {
         key = 'soriancheat',
-        name = "AIx: SAI 1.7.3c",
+        name = "AIx: SAI 1.7.3d",
     },
     {
         key = 'sorianrushcheat',
-        name = "AIx: SAI Rush 1.7.3c",
+        name = "AIx: SAI Rush 1.7.3d",
     },
     {
         key = 'sorianaircheat',
-        name = "AIx: SAI Air 1.7.3c",
+        name = "AIx: SAI Air 1.7.3d",
     },
     {
         key = 'sorianwatercheat',
-        name = "AIx: SAI Water 1.7.3c",
+        name = "AIx: SAI Water 1.7.3d",
     },
     {
         key = 'sorianturtlecheat',
-        name = "AIx: SAI Turtle 1.7.3c",
+        name = "AIx: SAI Turtle 1.7.3d",
     },
     {
         key = 'sorianadaptivecheat',
-        name = "AIx: SAI Adaptive 1.7.3c",
+        name = "AIx: SAI Adaptive 1.7.3d",
     },
 }
