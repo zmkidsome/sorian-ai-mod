ed9787b85ee95c79a2d0d07d2ca797cb8c7b549a
merobbins <merobbins@f2e18c2a-3941-0410-a48b-ebcc20576c27>
2008-12-01 02:26:37 +0000

    Fixed another bug.

--- trunk/lua/AI/CustomAIs/SorianAI.lua
+++ trunk/lua/AI/CustomAIs/SorianAI.lua
@@ -9,53 +9,53 @@
 AIList = {
     {
         key = 'sorian',
-        name = "AI: SAI 1.7.9b",
+        name = "AI: SAI 1.7.9c",
     },
     {
         key = 'sorianrush',
-        name = "AI: SAI Rush 1.7.9b",
+        name = "AI: SAI Rush 1.7.9c",
     },
     {
         key = 'sorianair',
-        name = "AI: SAI Air 1.7.9b",
+        name = "AI: SAI Air 1.7.9c",
     },
     {
         key = 'sorianwater',
-        name = "AI: SAI Water 1.7.9b",
+        name = "AI: SAI Water 1.7.9c",
     },
     {
         key = 'sorianturtle',
-        name = "AI: SAI Turtle 1.7.9b",
+        name = "AI: SAI Turtle 1.7.9c",
     },
     {
         key = 'sorianadaptive',
-        name = "AI: SAI Adaptive 1.7.9b",
+        name = "AI: SAI Adaptive 1.7.9c",
     },
 }
 
 CheatAIList = {
     {
         key = 'soriancheat',
-        name = "AIx: SAI 1.7.9b",
+        name = "AIx: SAI 1.7.9c",
     },
     {
         key = 'sorianrushcheat',
-        name = "AIx: SAI Rush 1.7.9b",
+        name = "AIx: SAI Rush 1.7.9c",
     },
     {
         key = 'sorianaircheat',
-        name = "AIx: SAI Air 1.7.9b",
+        name = "AIx: SAI Air 1.7.9c",
     },
     {
         key = 'sorianwatercheat',
-        name = "AIx: SAI Water 1.7.9b",
+        name = "AIx: SAI Water 1.7.9c",
     },
     {
         key = 'sorianturtlecheat',
-        name = "AIx: SAI Turtle 1.7.9b",
+        name = "AIx: SAI Turtle 1.7.9c",
     },
     {
         key = 'sorianadaptivecheat',
-        name = "AIx: SAI Adaptive 1.7.9b",
+        name = "AIx: SAI Adaptive 1.7.9c",
     },
 }
